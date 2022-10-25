c152f1cad8bf0309e0289998de655fa5c3373f92
AEManov20 <aemanov20@codingburgas.bg>
2022-10-25 09:40:22 +0300

    Move GetFileContents to a seperate file_reader header

--- Engine/Source/SceneReader.cpp
+++ Engine/Source/SceneReader.cpp
@@ -1,51 +1,12 @@
 #include "SceneReader.hpp"
 #include "Managers/ECSManager.hpp"
 
-#include <fstream>
-#include <cassert>
-#include <sstream>
+#include "Components/TransformComponent2D.hpp"
+#include "Components/SpriteComponent.hpp"
+#include "Components/UIComponent.hpp"
 
-#include "./Components/TransformComponent2D.hpp"
-#include "./Components/SpriteComponent.hpp"
-#include "./Components/UIComponent.hpp"
-
+#include "Util/file_reader.hpp"
 #include "Util/json.hpp"
-
-/**
- * @brief Gets the contents of the .json file
- * 
- * @param path The path to the .json file
- * 
- * @return Returns a string with all the components on an entity
- */
-std::string GetFileContents(const std::string path)
-{
-    std::stringstream jsonStream;
-
-    std::ifstream fJson;
-
-    /**
-     * @brief Open the file specified in the path param
-     */
-    fJson.open(path, std::ios::in);
-
-    if (!fJson.is_open())
-        throw std::runtime_error("File \"" + path + "\" could not be opened");
-    
-    /**
-     * @brief While there is still information on the file, extract it. When there is no more information, close the file
-     */
-    std::string jsonLine;
-    while (std::getline(fJson, jsonLine))
-        jsonStream << jsonLine << '\n';
-    
-    fJson.close();
-
-    /**
-     * @brief Return the stringstream with the information on the file
-     */
-    return jsonStream.str();
-}
 
 void InitSceneFromFile(const std::string fPath)
 {
