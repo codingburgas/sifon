b1ee06f861d8bf695e689297c5af6caed082f2a9
AEManov20 <aemanov20@codingburgas.bg>
2022-10-31 11:49:55 +0200

    Implement UIBuilder interface Used to build UILayer(s)

---
+++ Engine/Source/Components/UIBuilder.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "../UIElement.hpp"
+#include "./UIComponent.hpp"
+#include "./Component.hpp"
+#include "../Managers/ECSManager.hpp"
+#include "../Managers/AppManager.hpp"
+#include "../Managers/EntityManager.hpp"
+
+// The UIBuilder is an interface for classes to create a UILayer and
+// populate them with their own UIElement(s).
+class UIBuilder : public Component
+{
+public:
+	DEFINE_COMPONENT(UIBuilder);
+
+	void OnCreate() override
+	{
+		auto ui = ECS::CreateComponent<UILayer>();
+		Build(ui);
+		boundEntity.lock()->AddComponent(ui);
+		boundEntity.lock()->RemoveComponent(ComponentName());
+	}
+
+	virtual void Build(std::shared_ptr<UILayer>& uiLayer) = 0;
+};
