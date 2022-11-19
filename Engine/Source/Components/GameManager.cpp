ce881d89c71b4d660c6a928faa10c3783b286a51
AEManov20 <aemanov20@codingburgas.bg>
2022-11-19 17:24:07 +0200

    Implement GameManager

---
+++ Engine/Source/Components/GameManager.cpp
@@ -0,0 +1,259 @@
+#include "./GameManager.hpp"
+#include <cmath>
+#include <algorithm>
+#include <ctime>
+#include <cstdlib>
+
+bool IsYearLeap(unsigned& year)
+{
+	return (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
+}
+
+unsigned GetMaxMonth()
+{
+	return 12;
+}
+
+unsigned GetMaxDayOfMonth(unsigned& month, unsigned& year)
+{
+	if (month == 2)
+	{
+		if (IsYearLeap(year)) return 29;
+		else return 28;
+	}
+	else if (month <= 7)
+	{
+		if (month % 2 == 0) return 30;
+		else return 31;
+	}
+	else
+	{
+		if (month % 2 == 0) return 31;
+		else return 30;
+	}
+}
+
+void GameManager::OnCreate()
+{
+	
+}
+
+void GameManager::OnUpdate()
+{
+	if (IsKeyPressed(KEY_ESCAPE))
+		SetPaused(!GetPaused());
+}
+
+
+void GameManager::AlterMoney(int by)
+{
+	m_State.m_Resources.m_Money += by;
+}
+
+void GameState::Date::SetDay(unsigned day)
+{
+	m_Day = std::clamp(day, 1u, GetMaxDayOfMonth(m_Month, m_Year));
+}
+
+void GameState::Date::SetMonth(unsigned month)
+{
+	m_Month = std::clamp(month, 1u, 12u);
+}
+
+void GameState::Date::SetYear(unsigned year)
+{
+	m_Year = std::clamp(year, 0u, UINT_MAX);
+}
+
+void GameState::Date::AdvanceDays(unsigned days)
+{
+	m_Day += days;
+	while (m_Day > GetMaxDayOfMonth(m_Month, m_Year))
+	{
+		m_Day -= GetMaxDayOfMonth(m_Month, m_Year);
+		AdvanceMonths(1);
+	}
+}
+
+void GameState::Date::AdvanceMonths(unsigned months)
+{
+	m_Month += months;
+	while (m_Month > 12)
+	{
+		m_Month -= 12;
+		AdvanceYears(1);
+	}
+}
+
+void GameState::Date::AdvanceYears(unsigned years)
+{
+	m_Year += years;
+}
+
+unsigned GameState::Date::GetMonth()
+{
+	return m_Month;
+}
+
+unsigned GameState::Date::GetYear()
+{
+	return m_Year;
+}
+
+unsigned GameState::Date::GetDay()
+{
+	return m_Day;
+}
+
+float GameManager::GetTodayWeather()
+{
+	unsigned year = m_State.m_Date.GetYear();
+	unsigned month = m_State.m_Date.GetMonth();
+	unsigned day = m_State.m_Date.GetDay();
+
+	float monthAndDays = static_cast<float>(month) + static_cast<float>(day) / static_cast<float>(GetMaxDayOfMonth(month, year));
+	return abs(sin((monthAndDays / 12) * PI) * 25);
+}
+
+void GameManager::LoadCharacterFromMetadataIndex(size_t index)
+{
+	// get metadata
+	auto& metadata = GET_COMPONENT_FROM(
+		EntityManager::GetInstance()->GetEntityFromTagName("MappackMetadata"),
+		MappackMetadataStore)->GetMetadata();
+
+	if (!(metadata.m_Characters.size() > index))
+		throw std::runtime_error("Index is out of bounds of the metadata character array");
+
+	auto character = metadata.m_Characters.at(index);
+	m_State.m_CharacterName = character.m_Name;
+	for (auto& [type, amount] : character.m_Boosts)
+		m_State.m_FactorScales[type] = amount;
+}
+
+bool GameManager::LoadSavefile()
+{
+	using namespace nlohmann;
+
+	if (!FileExists("save.file"))
+		return false;
+
+	auto json = json::parse(GetFileContents("save.file"));
+	if (!json.is_object())
+		return false;
+
+	if (!json["organisations"].is_array())
+		return false;
+
+	for (auto& organisation : json["organisations"])
+		m_State.m_RegionOrganisations.push_back(organisation.get<std::string>());
+
+	int i = 0;
+	for (auto& factorScale : json["factorScales"])
+	{
+		m_State.m_FactorScales[i] = factorScale;
+		if (i > FACTORS_COUNT - 1)
+			break;
+		i++;
+	}
+
+	json["factorsAmount"][Metadata::Factor::CLOTHES].get_to(m_State.m_Resources.m_Clothes);
+	json["factorsAmount"][Metadata::Factor::FOOD_WATER].get_to(m_State.m_Resources.m_Food);
+	json["factorsAmount"][Metadata::Factor::HOPE].get_to(m_State.m_Community.m_HopePercent);
+	json["factorsAmount"][Metadata::Factor::LOYALTY].get_to(m_State.m_Community.m_LoyaltyPercent);
+	json["factorsAmount"][Metadata::Factor::MANPOWER].get_to(m_State.m_Resources.m_Manpower);
+	json["factorsAmount"][Metadata::Factor::MELEE].get_to(m_State.m_Resources.m_MeleeAmount);
+	json["factorsAmount"][Metadata::Factor::MONEY].get_to(m_State.m_Resources.m_Money);
+	json["factorsAmount"][Metadata::Factor::MORAL].get_to(m_State.m_Community.m_MoralPercent);
+
+	return true;
+}
+
+bool GameManager::SaveSavefile()
+{
+	return true;
+}
+
+void GameManager::SetPaused(bool isPaused)
+{
+	m_State.m_IsPaused = isPaused;
+}
+
+bool GameManager::GetPaused()
+{
+	return m_State.m_IsPaused;
+}
+
+void GameManager::SetCharacterName(std::string name)
+{
+	m_State.m_CharacterName = name;// TODO: implement
+}
+
+void GameManager::SetFactorScales(std::array<float, FACTORS_COUNT> factors)
+{
+	m_State.m_FactorScales = factors;
+}
+
+void GameManager::MakeOrganisationInRegion(std::string region)
+{
+	m_State.m_Date.AdvanceMonths(2);
+	auto& regions = m_State.m_RegionOrganisations;
+	if (std::find(regions.begin(), regions.begin(), region) != regions.end())
+	{
+		regions.push_back(region);
+	}
+}
+
+int GameManager::GetMoneyAmount()
+{
+	return m_State.m_Resources.m_Money;
+}
+
+int GameManager::GetResourceAmount(GameState::Resource resourceType)
+{
+	switch (resourceType)
+	{
+	case GameState::Resource::CLOTHES:
+		return m_State.m_Resources.m_Clothes;
+	case GameState::Resource::FOOD_WATER:
+		return m_State.m_Resources.m_Food;
+	case GameState::Resource::MANPOWER:
+		return m_State.m_Resources.m_Manpower;
+	case GameState::Resource::MELEE:
+		return m_State.m_Resources.m_MeleeAmount;
+	}
+
+	return -1;
+}
+
+float GameManager::GetCommunityPercent(GameState::CommunityResource resourceType)
+{
+	switch (resourceType)
+	{
+	case GameState::CommunityResource::HOPE:
+		return m_State.m_Community.m_HopePercent;
+	case GameState::CommunityResource::LOYALTY:
+		return m_State.m_Community.m_LoyaltyPercent;
+	case GameState::CommunityResource::MORAL:
+		return m_State.m_Community.m_MoralPercent;
+	}
+
+	return -1;
+}
+
+float GameManager::GetWinProbability()
+{
+	
+}
+
+void GameManager::MakeRevolutionInRegion(std::string region)
+{
+	// TODO: implement
+	// NOTE: use GetWinProbability
+}
+
+void GameManager::BuyResources(GameState::Resource resourceType, int amount)
+{
+	// TODO: implement
+	// NOTE: use GetMoneyAmount to compare, AlterMoney
+}
---
+++ Engine/Source/Components/GameManager.hpp
@@ -0,0 +1,127 @@
+#pragma once
+
+#include "./Component.hpp"
+#include "./MappackMetadataStore.hpp"
+#include "./Component.hpp"
+#include "../Managers/EntityManager.hpp"
+#include "../Util/json.hpp"
+#include "../Util/file_reader.hpp"
+
+#include "raylib.h"
+
+#include <array>
+
+struct GameState
+{
+	std::array<float, FACTORS_COUNT> m_FactorScales;
+	std::string m_CharacterName;
+	std::string m_CountryCurrency;
+
+	std::vector<std::string> m_RegionOrganisations;
+
+	enum class Resource
+	{
+		MANPOWER = 2,
+		MELEE = 3,
+		FOOD_WATER = 6,
+		CLOTHES = 7
+	};
+
+	enum class CommunityResource
+	{
+		MORAL = 0,
+		LOYALTY = 1,
+		HOPE = 4,
+	};
+
+	struct {
+		std::string m_Region;
+		bool m_IsActive = false;
+	} m_CurrentRevolution;
+
+	int m_WonRevolutionsCount = 0;
+	int m_LostRevolutionsCount = 0;
+
+	struct {
+		int m_MeleeAmount = 0;
+		int m_Manpower = 0;
+		int m_Money = 0;
+		int m_Food = 0;
+		int m_Clothes = 0;
+	} m_Resources;
+
+	struct {
+		float m_LoyaltyPercent = 0.f;
+		float m_MoralPercent = 0.f;
+		float m_HopePercent = 0.f;
+	} m_Community;
+
+	struct Date {
+	private:
+		unsigned m_Year;
+		unsigned m_Month;
+		unsigned m_Day;
+	public:
+		Date(unsigned day, unsigned month, unsigned year)
+		{
+			SetDay(day);
+			SetMonth(month);
+			SetYear(year);
+		}
+
+		void SetDay(unsigned day);
+		void SetYear(unsigned year);
+		void SetMonth(unsigned month);
+
+		void AdvanceMonths(unsigned count);
+		void AdvanceDays(unsigned count);
+		void AdvanceYears(unsigned count);
+
+		unsigned GetMonth();
+		unsigned GetYear();
+		unsigned GetDay();
+	} m_Date{1u, 1u, 1u}, m_EndingDate{1u, 1u, 1u};
+
+	bool m_IsPaused = false;
+};
+
+class GameManager : public Component
+{
+	GameState m_State;
+
+	// Helper functions
+	// Money
+	void AlterMoney(int by);
+
+public:
+	DEFINE_COMPONENT(GameManager);
+
+	void OnCreate() override;
+	void OnUpdate() override;
+
+	void SetCharacterName(std::string name);
+	void SetFactorScales(std::array<float, FACTORS_COUNT> factors);
+	void LoadCharacterFromMetadataIndex(size_t index);
+
+	
+	float GetTodayWeather(); /* in celsius degrees */
+
+	// Resources
+	int GetMoneyAmount(); /* in currency units */
+	int GetResourceAmount(GameState::Resource resourceType); /* in units */
+	float GetCommunityPercent(GameState::CommunityResource resourceType); /* in percent */
+
+	float GetWinProbability(); /* in percent */
+
+	// Actions
+	void MakeOrganisationInRegion(std::string region);
+	void MakeRevolutionInRegion(std::string region);
+	void BuyResources(GameState::Resource resourceType, int amount);
+
+	void SetPaused(bool isPaused);
+	bool GetPaused();
+
+	// NOT IMPLEMENTED
+	bool LoadSavefile();
+	bool SaveSavefile();
+};
