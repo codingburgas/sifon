92b8a6b8aeb7820e3fb993df0ecc4917b4dd92b1
AEManov20 <aemanov20@codingburgas.bg>
2022-11-19 17:20:19 +0200

    Changes in Button class

--- Engine/Source/UI/Button.cpp
+++ Engine/Source/UI/Button.cpp
@@ -1,7 +1,10 @@
 #include "Button.hpp"
 
-Vector2 Button::m_Padding = { 30.f, 10.f };
-float Button::m_RoundRadius = 10.f;
+Vector2 Button::s_Padding = { 30.f, 10.f };
+float Button::s_RoundRadius = 10.f;
+float Button::s_TransparencyWhenNotHovered = .7f;
+float Button::s_TransparencyWhenHovered = 1.f;
+float Button::s_InterpTime = .2f;
 
-float Button::m_FontSize = 40.f;
-const char* Button::m_FontPath = "res/fonts/Inter/Inter-ExtraBold.ttf";
+float Button::s_FontSize = 40.f;
+const char* Button::s_FontPath = "res/fonts/Inter/Inter-ExtraBold.ttf";
--- Engine/Source/UI/Button.hpp
+++ Engine/Source/UI/Button.hpp
@@ -1,35 +1,58 @@
 #pragma once
 
 #include "./UIElement.hpp"
+#include "reasings.h"
 
 #include <string>
 
 class Button : public UIElement
 {
-	static Vector2 m_Padding;
-	static float m_RoundRadius;
+public:
+	static Vector2 s_Padding;
+	static float s_RoundRadius;
 
-	static float m_FontSize;
-	static const char* m_FontPath;
-	
+	static float s_FontSize;
+	static float s_TransparencyWhenNotHovered;
+	static float s_TransparencyWhenHovered;
+	static float s_InterpTime;
+	static const char* s_FontPath;
+
+protected:
 	std::string m_Text;
 	Font m_Font;
 	Color m_Color;
+	float m_Transparency = s_TransparencyWhenNotHovered;
+	float m_Time = 0.f;
 
 public:
-	Button(std::string text, Color color) : m_Text(text), m_Color(color), m_Font(LoadFontEx(m_FontPath, m_FontSize, nullptr, 0))
+	Button(std::string text, Color color) : m_Text(text), m_Color(color), m_Font(LoadFontEx(s_FontPath, s_FontSize, nullptr, 0))
 	{}
 
-	~Button()
+	virtual ~Button()
 	{
 		UnloadFont(m_Font);
 	}
 
-	void Draw() override
+	virtual void Draw() override
 	{
+		if (IsMouseInButton())
+		{
+			m_Transparency = EaseSineInOut(m_Time, s_TransparencyWhenNotHovered, s_TransparencyWhenHovered - s_TransparencyWhenNotHovered, s_InterpTime);
+			if (m_Time < s_InterpTime)
+				m_Time += GetFrameTime();
+		}
+		else
+		{
+			m_Transparency = EaseSineInOut(m_Time, s_TransparencyWhenNotHovered, s_TransparencyWhenHovered - s_TransparencyWhenNotHovered, s_InterpTime);
+			if (m_Time > 0.f)
+				m_Time -= GetFrameTime();
+		}
+
 		auto size = GetSize();
-		DrawRectangleRounded({ m_Position.x, m_Position.y, size.x, size.y }, m_RoundRadius, 10, m_Color);
-		DrawTextEx(m_Font, m_Text.c_str(), Vector2{ m_Position.x + m_Padding.x, m_Position.y + m_Padding.y }, static_cast<float>(m_FontSize), 1.f, WHITE);
+		DrawRectangleRounded({ m_Position.x, m_Position.y, size.x, size.y },
+			s_RoundRadius, 10, Color { m_Color.r, m_Color.g, m_Color.b, static_cast<unsigned char>(m_Transparency * 255) });
+		DrawTextEx(m_Font, m_Text.c_str(), Vector2{ m_Position.x + s_Padding.x, m_Position.y + s_Padding.y }, static_cast<float>(s_FontSize), 1.f,
+			{ 255, 255, 255, static_cast<unsigned char>(m_Transparency * 255) });
 	}
 
 	bool IsMouseInButton()
@@ -43,8 +66,8 @@ public:
 		return IsMouseInButton() && IsMouseButtonReleased(MOUSE_BUTTON_LEFT);
 	}
 
-	Vector2 GetSize() override
+	virtual Vector2 GetSize() override
 	{
-		return { MeasureTextEx(m_Font, m_Text.c_str(), m_FontSize, 1.f).x + m_Padding.x * 2.f, m_FontSize + m_Padding.y * 2.f };
+		return { MeasureTextEx(m_Font, m_Text.c_str(), s_FontSize, 1.f).x + s_Padding.x * 2.f, s_FontSize + s_Padding.y * 2.f };
 	}
 };
